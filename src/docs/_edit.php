f793682a8d1034abfd44776cc069c923500f1a59
the.codeartistic.ninja <codeartisticninja@users.noreply.github.com>
2016-06-02 14:51:36 +0200

    Smarter docs editor

--- src/docs/_edit.php
+++ src/docs/_edit.php
@@ -3,8 +3,13 @@
     if (@$_POST["body"]) {
       $body = str_replace("\r\n", "\n", $_POST["body"]);
       file_put_contents(basename($_GET["file"]), $body);
-      header("Refresh: 1; url=../../build/docs/".basename($_GET["file"], ".md").".html");
-      die("<h1>Saved!");
+      if (@$_GET["get"] === "json") {
+        header("Content-Type: application/json");
+        die('{ "success":true, "status": "ok" }');
+      } else {
+        header("Refresh: 1; url=../../build/docs/".basename($_GET["file"], ".md").".html");
+        die("<h1>Saved!");
+      }
     } else {
       $body = file_get_contents(basename($_GET["file"]));
     }
@@ -22,7 +27,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $_GET["file"]; ?></title>
+    <title><?php echo @$_GET["file"]?$_GET["file"]:"./"; ?></title>
     <link rel="stylesheet" type="text/css" href="../../build/style/markdown.css">
   </head>
   <body>
@@ -38,5 +43,8 @@
         <p><input type="submit" value="Save" /></p>
       </form>
     <?php endif; ?>
+    <script type="text/javascript"> var filename = "<?php echo basename($_GET["file"]); ?>"; </script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+    <script type="text/javascript" src="_edit.js"></script>
   </body>
 </html>
